1512edfdbbccffb99eb1a2e1cd15777509965fd3
Adrian <adrian.todirica7@hotmail.com>
2019-07-08 13:41:14 +0300

    Added account

---
+++ src/main/java/com/montran/banking/account/persistence/AccountRepository.java
@@ -0,0 +1,11 @@
+package com.montran.banking.account.persistence;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.montran.banking.account.domain.entity.Account;
+
+@Repository
+public interface AccountRepository extends CrudRepository<Account, Long> {
+
+}
