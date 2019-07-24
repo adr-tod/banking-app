93f6d7f017e876d819d4fb08195d329364b09f23
Adrian <adrian.todirica7@hotmail.com>
2019-07-24 13:29:55 +0300

    Added spring security template backend

---
+++ src/main/java/com/montran/banking/security/JwtUserDetailsService.java
@@ -0,0 +1,27 @@
+package com.montran.banking.security;
+
+import java.util.ArrayList;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Component;
+
+import com.montran.banking.user.persistence.UserRepository;
+
+@Component
+public class JwtUserDetailsService implements UserDetailsService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Override
+	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+//		com.montran.banking.user.domain.entity.User user = userRepository.findByUsername(username);
+//		return new User(user.getUsername(), user.getPassword(), null);
+		return new User("javainuse", "$2a$10$slYQmyNdGzTn7ZLBXBChFOC9f6kFjAqPhccnP6DxlWXx2lPk1C3G6", new ArrayList<>());
+	}
+
+}
