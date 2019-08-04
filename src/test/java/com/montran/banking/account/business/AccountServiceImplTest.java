94f61615bd6df3b310c9354cdb54c9599d417a3c
Adrian <adrian.todirica7@hotmail.com>
2019-08-04 23:57:44 +0300

    Started account unit testing

---
+++ src/test/java/com/montran/banking/account/business/AccountServiceImplTest.java
@@ -0,0 +1,11 @@
+package com.montran.banking.account.business;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServiceImplTest {
+
+	@Autowired
+	private AccountService accountService;
+}
