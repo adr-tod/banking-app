f60c337a30e91dbe3c14a4133c6deacef0b72b5f
Adrian <adrian.todirica7@hotmail.com>
2019-08-06 00:35:03 +0300

    Continued payment unit testing

--- src/test/java/com/montran/banking/payment/business/PaymentServiceImplTest.java
+++ src/test/java/com/montran/banking/payment/business/PaymentServiceImplTest.java
@@ -1,6 +1,8 @@
 package com.montran.banking.payment.business;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
@@ -12,6 +14,8 @@ import org.springframework.security.test.context.support.WithMockUser;
 import com.montran.banking.account.business.AccountService;
 import com.montran.banking.account.domain.dto.AccountCreateDTO;
 import com.montran.banking.account.domain.entity.Account;
+import com.montran.banking.account.persistence.AccountRepository;
+import com.montran.banking.accountstatus.persistence.AccountStatusRepository;
 import com.montran.banking.payment.domain.dto.PaymentCreateDTO;
 import com.montran.banking.payment.domain.dto.PaymentVerifyDTO;
 import com.montran.banking.payment.domain.entity.Payment;
@@ -36,7 +40,13 @@ class PaymentServiceImplTest {
 	private PaymentRepository paymentRepository;
 
 	@Autowired
+	private AccountRepository accountRepository;
+
+	@Autowired
 	private PaymentStatusRepository paymentStatusRepository;
+
+	@Autowired
+	private AccountStatusRepository accountStatusRepository;
 
 	private Account account1;
 	private Account account2;
@@ -165,4 +175,54 @@ class PaymentServiceImplTest {
 		// cleanup
 		paymentService.deleteById(payment.getId());
 	}
+
+	@Test
+	public void testApproveUserToApproveAlsoCreateShouldThrowException() {
+		// setup
+		Payment payment = paymentService
+				.create(new PaymentCreateDTO(account1.getIban(), account2.getIban(), 1.0, "EUR"));
+		payment.setCreatedBy(userService.findByUsername("igibson"));
+		payment.setVerifiedBy(userService.findByUsername("sean.feeney"));
+		payment.setStatus(paymentStatusRepository.findByName("APPROVE"));
+		paymentRepository.save(payment);
+		// approve payment by the one who created it
+		assertThrows(RuntimeException.class, () -> paymentService.approve(payment.getId()));
+		// cleanup
+		paymentService.deleteById(payment.getId());
+	}
+
+	@Test
+	public void testApproveUserToApproveAlsoVerifyShouldThrowException() {
+		// setup
+		Payment payment = paymentService
+				.create(new PaymentCreateDTO(account1.getIban(), account2.getIban(), 1.0, "EUR"));
+		payment.setCreatedBy(userService.findByUsername("skuhn"));
+		payment.setVerifiedBy(userService.findByUsername("igibson"));
+		payment.setStatus(paymentStatusRepository.findByName("APPROVE"));
+		paymentRepository.save(payment);
+		// approve payment by the one who verified it
+		assertThrows(RuntimeException.class, () -> paymentService.approve(payment.getId()));
+		// cleanup
+		paymentService.deleteById(payment.getId());
+	}
+
+	@Test
+	public void testApproveAccountCannotDebitShouldThrowException() {
+		// setup
+		account1.getBalance().setAvailable(5.0);
+//		account1.setStatus(accountStatusRepository.findByName("BLOCKED").get());
+		accountRepository.save(account1);
+		Payment payment = paymentService
+				.create(new PaymentCreateDTO(account1.getIban(), account2.getIban(), 1.0, "EUR"));
+		payment.setCreatedBy(userService.findByUsername("skuhn"));
+		payment.setVerifiedBy(userService.findByUsername("sean.feeney"));
+		payment.setStatus(paymentStatusRepository.findByName("APPROVE"));
+		paymentRepository.save(payment);
+		// approve payment with debit account blocked
+		assertThrows(RuntimeException.class, () -> paymentService.approve(payment.getId()));
+		// cleanup
+		paymentService.deleteById(payment.getId());
+	}
+
+	// ACTIVATE CODE ASSIST FOR .
 }
